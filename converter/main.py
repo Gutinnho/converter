8a87d5da727da45f3d9099fa1f51b809be819ced
Gutinnho <glunardi99@gmail.com>
2024-03-04 18:26:34 -0300

    feat(main): Add the rest of the logic for all options
    - Add the logic to get user input, convert it, and then display the result
    - Add the logic for when the option is valid and when it is invalid
    - Add the exiting function to close the program
    - Add the ask_continue function to confirm the user's decision to continue

--- converter/main.py
+++ converter/main.py
@@ -7,9 +7,9 @@ from .converter import Converter, make_converters
 def main():
     converters = make_converters()
 
-    click.clear()
     option = "main"
 
+    click.clear()
     while True:
         if option == "main":
             menu(converters)
@@ -38,17 +38,71 @@ def menu(converters: List[Converter]) -> None:
 
 def chosse_option(option: str, converters: List[Converter]):
     if option == "q":
-        exit()
+        exiting()
 
     for converter in converters:
         if converter.abbr == option:
-            click.clear()
-            click.pause(
-                click.style(f"Option selected: {converter.base_name}", fg="green")
-            )
-            click.clear()
+            valid_option(converter)
+            ask_continue()
             return "main"
 
-    click.pause(click.style("Invalid option", bold=True, fg="red"))
-    click.clear()
+    invalid_option()
     return "main"
+
+
+def valid_option(converter: Converter):
+    click.clear()
+    while True:
+        try:
+            click.clear()
+            number = click.prompt(
+                click.style(f"{converter.base_name}:", fg=converter.color),
+                prompt_suffix=" ",
+            )
+            number_int = converter.validate(number)
+            break
+        except click.BadParameter as e:
+            click.pause(click.style(e, fg="red"), err=True)
+
+    result = converter.convert(number_int)
+
+    click.clear()
+
+    colors = ["red", "green", "blue", "magenta"]
+    for index, (base_name, value) in enumerate(result.items()):
+        click.secho(f"{base_name}: ", fg=colors[index], nl=False)
+        click.secho(value)
+    click.echo()
+
+    click.pause("...")
+
+
+def invalid_option():
+    click.pause(click.style("Invalid Option", fg="red"), err=True)
+    click.clear()
+
+
+def exiting():
+    click.clear()
+    click.secho("Exiting!", bold=True, fg="white")
+    exit()
+
+
+def ask_continue():
+    while True:
+        click.clear()
+        click.secho("Continue?", nl=True, bold=True, fg="white")
+
+        option = click.getchar().lower()
+        if option == "y":
+            click.clear()
+            break
+        elif option == "n":
+            exiting()
+        else:
+            click.echo()
+            click.secho("Invalid option", bold=True, fg="red")
+            click.pause(
+                click.style("Please type 'y' or 'n'", fg="red"),
+                err=True,
+            )
