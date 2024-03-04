bfad94a531e204224a2fba7180fb219e7cb8d234
Gutinnho <glunardi99@gmail.com>
2024-03-04 17:50:47 -0300

    feat: Add the menu function
    - Add the menu function to display the options

--- converter/main.py
+++ converter/main.py
@@ -1,5 +1,28 @@
 import click
+from typing import List
+
+from .converter import Converter, make_converters
 
 
 def main():
-    click.echo("Main")
+    converters = make_converters()
+    click.clear()
+    menu(converters)
+
+
+def menu(converters: List[Converter]) -> None:
+    click.secho("Select a numeric system", bold=True, fg="white")
+
+    for i, converter in enumerate(converters, start=1):
+        click.secho(
+            f"{ '└──  ' if len(converters) - i == 0 else '├──  '}",
+            nl=False,
+            fg=converter.color,
+        )
+        click.secho(f"{converter.abbr} - ", fg=converter.color, nl=False)
+        click.echo(converter.base_name)
+
+    for _ in range(2):
+        click.echo()
+    click.secho("q - quit", fg="red")
+    click.echo()
